1bdc63679290caf32387548246bb35f385368192
sakeef <sakeef@thinkstud.io>
2022-11-24 23:44:54 +0500

    Project Init

--- Account.java
+++ Account.java
@@ -1,32 +1,17 @@
 package newbank.server;
 
 public class Account {
+	
+	private String accountName;
+	private double openingBalance;
 
-    private String accountName;
-
-    private String accountType;
-    private double openingBalance;
-
-    //Set account type to 1-"CURRENT", 2-"SAVING" or 3-"MULTI"
-    public Account(String accountName, double openingBalance, int type) {
-        this.accountName = accountName;
-        this.openingBalance = openingBalance;
-        this.accountType = type;
-    }
-
-    public String toString() {
-        return (accountName + ": " + openingBalance);
-    }
-
-    public String getType(){
-        switch (accountType){
-            case 1:
-                return "This is a current account.";
-            case 2:
-                return "This is a savings account.";
-            case 3:
-                return "This is a multi-currency account";
-        }
-    }
+	public Account(String accountName, double openingBalance) {
+		this.accountName = accountName;
+		this.openingBalance = openingBalance;
+	}
+	
+	public String toString() {
+		return (accountName + ": " + openingBalance);
+	}
 
 }
